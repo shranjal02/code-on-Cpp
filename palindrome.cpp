c9deb821b421a0a06c6f313078ea9b6cf4534013
Shranjal Agrawal <101035605+shranjalagrawal2@users.noreply.github.com>
2022-09-01 13:07:32 -0700

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,29 +1,72 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+class node
+{
+public:
+    int data;
+    node *next;
+    // constructor
 
-    int n;
-    cin >>n;
-    char arr[n+1];
-    cin>>arr;
-
-    bool check=1;
-
-    for(int i=0; i<n; i++){
-        if(arr[i] != arr[n-1-i]){
-            check= 0;
-            break;
-        }
-    }
-    if (check ==true){
-        cout <<"palindrome"<<endl;
-
-    }
-    else
+    node(int data)
     {
-        cout<<"not palindrome"<<endl;
+        this->data = data;
+        this->next = NULL;
+    }
+};
+void insert(node *&head, int data)
+{
+    node *temp = new node(data);
+    temp->next = head;
+    head = temp;
+}
+
+node *reverse(node *&head){
+    if(head == NULL || head -> next == NULL){
+        return head;
+    }
+    node* newNode = reverse(head -> next);
+    head -> next -> next = head;
+    head -> next = NULL;
+    return newNode;
+}
+
+bool isPalin(node *&head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return true;
+    }
+    node *slow = head;
+    node *fast = head;
+
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
     }
 
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+    node *dummy = head;
+
+    while (slow != NULL)
+    {
+        if (dummy->data != slow->data)
+        {
+            return false;
+        }
+        dummy = dummy->next;
+        slow = slow->next;
+    }
+    return true;
+}
+
+int main()
+{
+    node *head = new node(1);
+    insert(head, 2);
+    insert(head, 1);
+    isPalin(head) ? cout << "true" : cout << "false";
     return 0;
 }
